4e390d8bed0d24909626c0f7b78e9b167494fcdb
DreadIsBack <ragnaros-alex@rambler.ru>
2014-06-30 15:35:56 +0300

    Test working of shader and storage buffer

--- Data/rays.cs.glsl
+++ Data/rays.cs.glsl
@@ -24,6 +24,16 @@ layout(RGBA32F) uniform writeonly image2D outImage;
 layout (local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 void main()
 {
-	vec3 color = vec3(0.3, 1.0, 0.3);
+	vec3 color = vec3(0.3, 1.0, 0.3); // background color
+
+	for (int i = 0; i < gNumQuads; i++)
+	{
+		if (gl_GlobalInvocationID.x >= quads[i].a.x && gl_GlobalInvocationID.x <= quads[i].c.x &&
+			gl_GlobalInvocationID.y >= quads[i].a.y && gl_GlobalInvocationID.y <= quads[i].c.y)
+		{
+			color = quads[i].diffuse.xyz;
+		}
+	}
+
 	imageStore(outImage, ivec2(gl_GlobalInvocationID.xy), vec4(color, 1.0f));
 }
